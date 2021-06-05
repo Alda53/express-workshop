37cd3f47d250e8f792df9cd01bad5707b1118fb5
Alda <caldair99@gmai.com>
2021-06-04 21:05:49 -0500

    Pokedex added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 //express aquí es uma clase, require busca una dependencia instalada, no necesita ruta
 const express = require('express')
 const app = express()
+const { pokemon } = require('./pokedex.json')
 
 //req almacena la info del URL, res respuesta que damos, next 
 app.get("/",(req, res, next) => {
@@ -10,10 +11,14 @@ app.get("/",(req, res, next) => {
 });
 
 //los dos puntos en la direccion indica el nombre de una variable
-app.get("/:name",(req, res, next) => {
-    res.status(200) //código q indica q todo se ejecutó bien
-    //Vemos el valor de la variabvle de la URL
-    res.send("Hola, " + req.params.name)
+app.get("/pokemon",(req, res, next) => {
+    res.status(200)
+    res.send(pokemon)
+});
+
+app.get("/pokemon/:id",(req, res, next) => {
+    res.status(200)
+    res.send(pokemon[req.params.id - 1])
 });
 //montamos un servidor
 
